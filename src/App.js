1d654de08bb220567046429a5c403a433ce046b4
gavrushuk <havryschuk@gmail.com>
2022-12-28 18:00:28 +0200

    weather app resonsive and fixes

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ const weatherIconBaseUrl = 'https://openweathermap.org/img/wn/';
  
 const App = () => {
   const [cityWeatherForecast, setCityWeatherForecast] = useState(null);
+  const [isErrorGerCityWeatherForecast, setIsErrorGerCityWeatherForecast] = useState(false);
   const [searchedValue, setSearchedValue] = useState([]);
 
   const searchCityWeatherForecast = async (city) => {
+    if (!city) return;
+
     const response = await fetch(`${weatherApiBaseUrl}${city}&unit=matrix&appid=007dc817717ddc58cf7f72a756e3af85`);
     const data = await response.json();
 
     if (data.cod === '404') {
       setCityWeatherForecast(null);
+      setIsErrorGerCityWeatherForecast(true);
       document.body.style.backgroundImage = `url(${randomBgImageBaseUrl}weather)`;
       return;
     }
 
     setCityWeatherForecast(data);
+    setIsErrorGerCityWeatherForecast(false);
     document.body.style.backgroundImage = `url(${randomBgImageBaseUrl}${city})`;
   }
 
@@ -37,9 +42,9 @@ const App = () => {
             type="text"
             placeholder="Search..."
             onChange={(e) => setSearchedValue(e.target.value)}
-            onKeyUp={(e) => e.key === 'Enter' ? searchCityWeatherForecast(searchedValue) : ''}
+            onKeyUp={(e) => e.key === 'Enter' ? searchCityWeatherForecast(searchedValue.trim()) : ''}
           />
-          <button onClick={() => searchCityWeatherForecast(searchedValue)} className="search-btn">
+          <button onClick={() => searchCityWeatherForecast(searchedValue.trim())} className="search-btn">
             <img src={searchIcon} alt="" />
           </button>
         </div>
@@ -49,7 +54,7 @@ const App = () => {
             (
               <div className="search-result">
                 <div className="search-result-title">Weather in {cityWeatherForecast.name}</div>
-                <div className="search-result-degree">{(+cityWeatherForecast.main?.temp - 273.15).toFixed(2)} &deg;C</div>
+                <div className="search-result-degree">{(+cityWeatherForecast.main?.temp - 273.15).toFixed(0)} &deg;C</div>
                 <div className="search-result-weather">
                   <img src={`${weatherIconBaseUrl}${cityWeatherForecast.weather && cityWeatherForecast.weather[0].icon}.png`} alt="" />
                   {cityWeatherForecast.weather && cityWeatherForecast.weather[0].main}
@@ -57,7 +62,8 @@ const App = () => {
                 <div className="search-result-humidity">Humidity: {cityWeatherForecast.main?.humidity}%</div>
                 <div className="search-result-wind-speed">Wind Speed: {cityWeatherForecast.wind?.speed} km/hr</div>
               </div>
-            ) : ''
+            ) : isErrorGerCityWeatherForecast ? 
+              <div className="result-not-found">City not found</div> : ''
         }
       </div>
     </div>
